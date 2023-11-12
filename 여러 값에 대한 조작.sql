37acb6e081bcacbd99335fb9fe3134dfd9908890
Gangmin Jeong <114273570+lastdancewithyou@users.noreply.github.com>
2023-11-12 10:26:43 +0900

    Update 여러 값에 대한 조작.sql

--- 여러 값에 대한 조작.sql
+++ 여러 값에 대한 조작.sql
@@ -157,6 +157,7 @@ SELECT
 	, CURRENT_DATE AS today
 	, register_stamp::date AS register_date
 	, birth_date::date AS birth_date
+	-- ::는 이전의 컬럼을 date 형식으로 바꿔주는 데이터 형식 캐스팅을 수행하는 연산자
 	, EXTRACT(YEAR FROM age(birth_date::date)) AS current_age
 	, EXTRACT(YEAR FROM age(register_stamp::date, birth_date::date)) AS register_age 
 FROM mst_users_with_dates
